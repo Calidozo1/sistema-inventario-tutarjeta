a0b7b17181d40688dd0c700f226220e75da370e6
Federico <javifede2005@gmail.com>
2025-11-10 20:38:55 -0400

    Pequeño cambio

--- frontend/frontend/src/app/app-routing-module.ts
+++ frontend/frontend/src/app/app-routing-module.ts
@@ -2,21 +2,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrarIncidenciaComponent } from './registrar-incidencia/registrar-incidencia.component';
+import { ConsultarIncidenciaComponent} from "./consultar-incidencia/consultar-incidencia.component";
 
 const routes: Routes = [
   {
     path: 'dashboard',
     loadComponent: () => import('./dashboard/dashboard').then(m => m.Dashboard)
   },
-  {
-    path: 'consultar-incidencia',
-    loadComponent: () =>
-      import('./consultar-incidencia/consultar-incidencia.component')
-        .then(m => m.ConsultarIncidenciaComponent)
-  },
   { path: 'registrar-incidencia', component: RegistrarIncidenciaComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: '**', redirectTo: 'dashboard' }
+  { path: '**', redirectTo: 'dashboard' },
+  {path : 'consultar-incidencia', component: ConsultarIncidenciaComponent }
 ];
 
 @NgModule({
